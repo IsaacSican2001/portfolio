b89e48f5ca99f44c63a3a5088a0dbc4fda3d2f0b
IsaacSican2001 <ic.isaacchristian.sican@cvsu.edu.ph>
2025-03-27 20:54:00 +0800

    redo

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './assets/styles/global.css';
-import './assets/styles/neon.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
