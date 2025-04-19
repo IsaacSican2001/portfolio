a1d3cf1a2e42a68d22894b93936fe1f25da4892e
IsaacSican2001 <ic.isaacchristian.sican@cvsu.edu.ph>
2025-04-19 15:35:46 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+// src/App.js
 import React from 'react';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar   from './components/Navbar';
+import Home     from './pages/Home';
+import About    from './pages/About';
+import Projects from './pages/Projects';    // ← new import
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-[#4b0f6b] to-[#701f91]">
-      <Navbar />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-gradient-to-b from-[#4b0f6b] to-[#701f91]">
+        <Navbar />
+
+        <Routes>
+          <Route path="/"         element={<Home />} />
+          <Route path="/about"    element={<About />} />
+          <Route path="/projects" element={<Projects />} />   {/* ← new route */}
+          {/* future pages: /contact, etc. */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
